dd34d64de26ae3ffea23ae974721b578d54c103d
alivejuicy <kkddhh1998@gmail.com>
2024-08-09 02:12:26 +0900

    #1 feat(main.py): 메인 컨트롤러단 설계

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 import uvicorn
-app = FastAPI()
+app = FastAPI(debug=True)
 
 
 @app.get("/")
@@ -12,5 +13,3 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000,  log_level="info")
